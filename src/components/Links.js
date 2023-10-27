9a3bc2c1d93b394d0e3b345ec744199910aa0ab0
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-27 09:50:18 +0300

    Links

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+
+function Links(props) {
+    console.log(props, 'links')
+    return (
+    <div>
+        <h3>Links</h3>
+        {/* <a href="#links">{props.links.github}</a> */}
+    </div>
+    )
+}
+
+export default Links;
