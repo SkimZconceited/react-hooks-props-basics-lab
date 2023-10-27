d8a8fc116b482745932243258a85d46aaf7360aa
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-27 16:26:40 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Links from "./Links";
 
-function About({bio, githubFn, linkedinFn}) {
+function About({bio = '', githubFn, linkedinFn}) {
   // console.log(userPass, bio, githubFn, linkedinFn);
   let isTrue = true;
   console.log(bio);
   return (
     <div id="about">
       <h2>About Me</h2>
-      {Boolean(bio) === isTrue ? <p>{bio}</p> : <p>{null}</p>}
+      {/* {Boolean(bio) === isTrue ? <p>{bio}</p> : <p>{null}</p>} */}
+      {bio && <p>{bio}</p>}
 
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
 
--- src/data/user.js
+++ src/data/user.js
@@ -1,7 +1,7 @@
 const user = {
   name: "Liza",
   city: "New York",
-  bio: null,
+  bio: "I made this!",
   color: "firebrick",
   links: {
     github: "https://github.com/liza",
